cdb824a98a2744b0033d17d821692b4f70e09126
waellaataoui <waellaataoui@rocketmail.com>
2020-05-09 14:12:48 +0100

    connect pg

--- src/settings.js
+++ src/settings.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 
 dotenv.config();
+console.log(process.env.CONNECTION_STRING);
 export const testEnvironmentVariable = process.env.TEST_ENV_VARIABLE;
 export const connectionString = process.env.CONNECTION_STRING;
