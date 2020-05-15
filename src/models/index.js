fb567cd96773fba975b0aab216b23e579e2924f6
waellaataoui <waellaataoui@rocketmail.com>
2020-05-15 12:57:14 +0100

    fixed travis build

--- src/models/index.js
+++ src/models/index.js
@@ -1,10 +1,11 @@
 'use strict';
-
+process.env.NODE_ENV = 'test'
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
+console.log(env);
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
